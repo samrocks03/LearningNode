13b6c590520366d167baef47be0b41a4f99882d7
Samarth Kulkarni <85547877+samrocks03@users.noreply.github.com>
2023-12-08 18:29:34 +0000

    basics

---
+++ app1.js
@@ -0,0 +1,14 @@
+const am = 13;
+if(am<10){
+    console.log("Small num");
+}else{
+    console.log("Large num");
+}
+
+/**
+ * __dirname   - path to current directory
+ * __filename  - file name
+ * require     - function to use modules ( commonJS )
+ * module      - info about current module (file )
+ * process     - info about where the program is being executed
+ */
